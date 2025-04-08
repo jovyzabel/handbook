512f217741346cf54bae731934ba83d7ec27d2f5
jovyzabel <97224435+jovyzabel@users.noreply.github.com>
2025-04-08 11:34:56 +0100

    Update filter.js

--- filter.js
+++ filter.js
@@ -9,7 +9,9 @@ function filter(array, callback) {
     
     return result;
   }
-  
+
+
     const numbers = [1, 2, 3, 4, 5];
+    // const evenNumbers = filter((number, index, array) => number % 2 === 0); 
     const evenNumbers = filter(numbers, num => num % 2 === 0);
     console.log(evenNumbers); // [2, 4]
